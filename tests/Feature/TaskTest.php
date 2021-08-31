a3b5a63a94949f382eb5b30f9a08fb91af7c374f
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2021-08-31 22:52:32 +0900

    add test

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function 一覧を取得()
+    {
+        $tasks = Task::factory()->count(10)->create();
+        $response = $this->getJson('api/tasks');
+
+        $response
+            ->assertOk()
+            ->assertJsonCount($tasks->count());
+    }
+}
